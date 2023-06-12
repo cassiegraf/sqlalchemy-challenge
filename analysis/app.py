9cca56b43fd8532ca53ab77007c3f9610b866759
cassiegraf <cassiegraf@gmail.com>
2023-06-11 17:07:26 -0700

    final python code

--- analysis/app.py
+++ analysis/app.py
@@ -28,7 +28,7 @@ def Climate_Data():
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/20140522<br/>"
-        f"/api/v1.0/startend"
+        f"/api/v1.0/Year2015"
     )
 
 #return last 12 months of precipitation data, include date and precipitation    
@@ -43,11 +43,7 @@ def precipitation():
 
     return jsonify(tobs)
 
-    ###NEED TO WORK ON THIS ^ It's not how the school is asking but the results are loading into webpage       
-
-
-
-#return a JSON list of stations from the dataset
+#return a JSON list of stations
 @app.route("/api/v1.0/stations")
 def stations():
     session = Session(engine)
@@ -71,39 +67,34 @@ def tob():
 
     return jsonify(tobs)
 
-
-# /api/v1.0/<start> and /api/v1.0/<start>/<end>
-# - return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified 
-#start or start-end range
-# - for a specified start, calculate 'TMIN','TAVG', and 'TMAX' for all the dates greater than or equal to the start date
-# - for a specified start date and end date, calculate 'TMIN', 'TAVG', and 'TMAX' for the dates from the start date to end date
-#inclusive
+#return a JSON list of min, avg, max temp for start date and start-end range
+#using start date of 5/22/14(20140522) and range of 1/1/2015-12/31/2015(Year2015)
 @app.route("/api/v1.0/20140522")
 def start():
 
     session = Session(engine)
 
-    results = session.query(measurement.date, func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).filter(measurement.date >= '2014-05-22').all()
+    results = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.\
+    max(measurement.tobs)).filter(measurement.date >= '2014-05-22').all()
     session.close()
 
     start_date = list(np.ravel(results))
 
-    return jsonify (start_date)
+    return jsonify(start_date)
 
-@app.route("/api/v1.0/startend")
+@app.route("/api/v1.0/Year2015")
 def start_end():
 
     session = Session(engine)
 
-    results = session.query(measurement.date, func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).filter(measurement.date >= '2014-05-22').filter(measurement.date <= '2015-05-22').all()
+    results = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
+    filter(measurement.date >= '2015-01-01', measurement.date <= '2015-12-31').all()
     session.close()
 
     start_end = list(np.ravel(results))
 
-    return jsonify (start_end)    
+    return jsonify(start_end)    
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
     
